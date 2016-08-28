125399aa39b51011a8bc685cbb15e3f85c2f190f
Marcel Wagenländer <marcel.wagenlaender@tum.de>
2016-08-28 17:26:54 +0200

    parses json file to a arff file and asignes random class

--- json_arff.js
+++ json_arff.js
@@ -8,24 +8,49 @@ function jsonToArff(file) {
     var data = fs.readFileSync(file, 'utf8');
     var json_data = JSON.parse(data);
 
-    var splited = file.split('.');
-    var name = splited[0];
+    var splitedFileName = file.split('.');
+    var fileName = splitedFileName[0];
 
     var arff = '';
-    arff = arff + '@RELATION ' + name + '\n' + '\n';
+    arff = arff + '@RELATION ' + fileName + '\n' + '\n';
 
     var firstElement = json_data[0];
     for (var key in firstElement) {
-        if (typeof firstElement[key] === 'string') {
-            arff = arff + '@ATTRIBUTE ' + key + ' string\n';
-        }
-        if (typeof firstElement[key] === 'number'
-                || typeof firstElement[key] === 'boolean') {
-            arff = arff + '@ATTRIBUTE ' + key + ' numeric\n';
-        }
+        //if (Object.hasOwnProperty(key)) {
+            if (typeof firstElement[key] === 'string') {
+                arff = arff + '@ATTRIBUTE ' + key + ' string\n';
+            }
+            if (typeof firstElement[key] === 'number'
+                    || typeof firstElement[key] === 'boolean') {
+                arff = arff + '@ATTRIBUTE ' + key + ' numeric\n';
+            }
+        //}
     }
-
     arff = arff + '@ATTRIBUTE class {+1,-1}\n\n'
 
-    fs.writeFileSync(name + '.arff', arff, 'utf8');
+    arff = arff + '@DATA\n';
+    for (var i = 0; i < json_data.length; i++) {
+        var element = json_data[i];
+        for (var key in element) {
+            //if (Object.hasOwnProperty(key)) {
+                if (typeof element[key] === 'string') {
+                    arff = arff + element[key].replace(/\s+/g, '') + ',';
+                } else {
+                    arff = arff + element[key] + ',';
+                }
+            //}
+        }
+        arff = arff + randomPlusMinus() + '\n';
+    }
+
+    fs.writeFileSync(fileName + '.arff', arff, 'utf8');
+}
+
+function randomPlusMinus() {
+    var number = Math.random();
+    if (number <= 0.5) {
+        return '-1';
+    } else {
+        return '+1';
+    }
 }
