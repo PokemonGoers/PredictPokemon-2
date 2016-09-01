5e567d66211392da5e64f6a45ab6b2bef341c9e1
MatthiasBaur <baurm@in.tum.de>
2016-09-01 14:21:50 +0200

    Concat on Json files

--- prediction.js
+++ prediction.js
@@ -1,12 +1,19 @@
 var fs = require('fs');
 var svm = require('node-svm');
 
-var data = fs.readFileSync('dummy1.json', 'utf8');
-var data2 = fs.readFileSync('dummy2.json', 'utf8');
-var data3 = fs.readFileSync('dummy3.json', 'utf8');
-var data4 = fs.readFileSync('dummy4.json', 'utf8');
+var data = fs.readFileSync('json/dummy1.json', 'utf8');
+var data2 = fs.readFileSync('json/dummy2.json', 'utf8');
+var data3 = fs.readFileSync('json/dummy3.json', 'utf8');
+var data4 = fs.readFileSync('json/dummy4.json', 'utf8');
 
 var json_data = JSON.parse(data);
+var json_data2 = JSON.parse(data2);
+var json_data3 = JSON.parse(data3);
+var json_data4 = JSON.parse(data4);
+
+json_data = json_data.concat(json_data2);
+json_data = json_data.concat(json_data3);
+json_data = json_data.concat(json_data4);
 
 var location_pokemon = [];
 
@@ -20,9 +27,14 @@ var classifier = new svm.CSVC({
     kFold:10
 });
 
-classifier.train(location_pokemon).done(function () {
-    location_pokemon.forEach(function (element) {
-        var prediction = classifier.predictSync(element[0]);
-        console.log('%d - %d -- %d', element[0][0], element[0][1], prediction);
+classifier.train(location_pokemon)
+    .progress(function(progress){
+        console.log('training progress: %d%', Math.round(progress*100));
+    })
+
+    .done(function () {
+        location_pokemon.forEach(function (element) {
+            var prediction = classifier.predictSync(element[0]);
+            console.log('%d - %d -- %d', element[0][0], element[0][1], prediction);
+        });
     });
-});
