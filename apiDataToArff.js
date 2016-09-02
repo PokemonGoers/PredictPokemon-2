47f6a844dbd93f88c7ebb89629c75db8cb8c77f0
aurel <aurelroci@hotmail.com>
2016-09-02 17:21:03 +0200

    api data to Json

---
+++ apiDataToArff.js
@@ -0,0 +1,98 @@
+/**
+ * Created by aurel on 02-Sep-16.
+ */
+var fs = require('fs');
+
+var source = 'json/convertedTimeApi.json';
+var destination = 'arff/apiData.arff';
+
+dataToArff(source, destination, 'pokemonID');
+
+function dataToArff(sources, destionation, classKey) {
+    var json_data = filesTojson(sources);
+    jsonToArff(json_data, classKey, 'apiData', destination);
+}
+
+function filesTojson(sources) {
+    var json_data = [];
+
+    sources.forEach(function (file) {
+        var data = fs.readFileSync(file, 'utf8');
+        json_data = json_data.concat(JSON.parse(data));
+    });
+
+    return json_data;
+}
+
+
+function jsonToArff(json_data, classKey, fileName, destination) {
+    var classLabels = allValuesForKeyInData(classKey, json_data);
+    var arff = '';
+    arff = arff + '@RELATION ' + fileName + '\n' + '\n';
+
+    var firstElement = json_data[0];
+    for (var key in firstElement) {
+        if (key !== classKey) {
+            if (typeof firstElement[key] === 'string') {
+                arff = arff + '@ATTRIBUTE ' + key + ' string\n';
+            }
+            if (typeof firstElement[key] === 'number'
+                || typeof firstElement[key] === 'boolean') {
+                arff = arff + '@ATTRIBUTE ' + key + ' numeric\n';
+            }
+        }
+    }
+    arff = arff + '@ATTRIBUTE class {' + classLabels.join(', ') + '}\n\n';
+
+    arff = arff + '@DATA\n';
+    for (var i = 0; i < json_data.length; i++) {
+        var element = json_data[i];
+        if (element["location"] !== null) {
+            // for (var key in element) {
+            //     if (key !== classKey) {
+            //         if (key === "location" && element[key] !== null) {
+            //             arff = arff + element[key]["coordinates"][0] + ',';
+            //             arff = arff + element[key]["coordinates"][1] + ',';
+            //
+            //         } else {
+            //             if (typeof element[key] === 'string') {
+            //                 arff = arff + element[key].replace(/\s+/g, '') + ',';
+            //             } else {
+            //                 arff = arff + element[key] + ',';
+            //             }
+            //         }
+            //     }
+            // }
+
+            arff = arff + element["_id"].replace(/\s+/g, '') + ',';
+            arff = arff + element["source"].replace(/\s+/g, '') + ',';
+            arff = arff + element[key]["coordinates"][0] + ',';
+            arff = arff + element[key]["coordinates"][1] + ',';
+            arff = arff + element["appearedOn"].replace(/\s+/g, '') + ',';
+            arff = arff + element["__v"] + ',';
+            arff = arff + element[classKey] + '\n';
+        }
+    }
+
+    fs.writeFileSync(destination, arff, 'utf8');
+}
+
+
+function getProperty(json, path) {
+    var tokens = path.split(".");
+    var obj = json;
+    for (var i = 0; i < tokens.length; i++) {
+        obj = obj[tokens[i]];
+    }
+    return obj;
+}
+
+function allValuesForKeyInData(key, json_data) {
+    var values = [];
+
+    json_data.forEach(function (row) {
+        values.push(row[key]);
+    });
+
+    return Array.from(new Set(values));
+}
