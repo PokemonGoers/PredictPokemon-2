309b6988836ced0a65a9538f49a1aefb161d9056
Benjamin Strobel <benjamin.strobel@tum.de>
2016-09-04 17:53:54 +0200

    added plot for pokemon sighting frequency

--- data_analysis/data_stats.py
+++ data_analysis/data_stats.py
@@ -30,24 +30,28 @@ def ntimes_sighted_counter(poke_dict, min_sight_count):
     return counter
 
 
-def plot_ntimes(poke_dict):
+def plot_ntimes(poke_dict, ax):
     stepsize = 5
     number_of_times = [n for n in range(5, 200, stepsize)]
     ntimes_counts = [ntimes_sighted_counter(poke_dict, n) / len(poke_dict) for n in number_of_times]
 
-    fig, ax = plt.subplots()
     ax.bar(number_of_times, ntimes_counts)
-
     ax.set_ylabel('% of Pokemons')
     ax.set_xlabel('n - number of times, step size %d' % stepsize)
     ax.set_title('Witnessed more than n times')
 
-    plt.show()
-
-
 def print_datapoints(poke_dict):
     datapoints = ['%d: %d' % (key, len(poke_dict[key])) for key in sorted(poke_dict.keys())]
     print('\n'.join(datapoints))
+
+def plot_datapoints(poke_dict, ax):
+    ids = [key for key in sorted(poke_dict.keys())]
+    datapoints = [len(poke_dict[key]) for key in sorted(poke_dict.keys())]
+    
+    ax.bar(ids, datapoints)
+    ax.set_ylabel('Number of sights')
+    ax.set_xlabel('Pokemon ID')
+    ax.set_title('Number of Pokemon datapoints')
 
 
 # how many of pokemons in our data set (in %) are witnessed more than 10 times,
@@ -55,14 +59,18 @@ def print_datapoints(poke_dict):
 # Best is to visualize this info as a cumulative plot
 # (x axis: number of times, y axis is % of the pokemons).
 
-filename = 'apiData.json'
-#filename = 'dummy.json'
+#filename = 'apiData.json'
+filename = 'dummy.json'
 
 with open(filename) as api_data:
     sights = json.load(api_data)
     sights = sights if filename == 'dummy.json' else filter_poke_data(sights)
     poke_dict = create_poke_dict(sights)
 
-    plot_ntimes(poke_dict)
-    print_datapoints(poke_dict)
+    ax1 = plt.subplot(2,1,1)
+    ax2 = plt.subplot(2,1,2)
 
+    print_datapoints(poke_dict)
+    plot_ntimes(poke_dict, ax1)
+    plot_datapoints(poke_dict, ax2)
+    plt.show()
