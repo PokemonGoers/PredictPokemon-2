d7a80cfc797c838cd6c20a257ac2683e68a64f95
Marcel Wagenländer <marcel.wagenlaender@tum.de>
2016-09-06 21:07:23 +0200

    changed getCity() because the name changes from city over town to village

--- latLonToCity.js
+++ latLonToCity.js
@@ -1,29 +1,40 @@
 const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var xmlhttp = new XMLHttpRequest();
 
-var latitude = 48.148626;
-var longitude = 11.568694;
+var latitude = 43.271338;
+var longitude = 6.638375;
 
-var address = latlngToAdress(latitude, longitude);
+var city = latLonToCity(latitude, longitude);
 
-function latlngToAdress(latitude, longitude) {
+console.log(city);
+
+function latLonToCity(latitude, longitude) {
     var url = 'http://nominatim.openstreetmap.org/reverse?format=json'
                 + '&lat=' + latitude + '&lon=' + longitude;
 
-    var address;
+    var city;
 
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             var data = JSON.parse(this.responseText);
-            address = getAdress(data);
+            city = getCity(data);
         }
     };
     xmlhttp.open("GET", url, false);
     xmlhttp.send();
 
-    return address;
+    return city;
 }
 
 function getCity(data) {
-    return data['address']['city'];
+    if (data['address']['city'] !== undefined) {
+        return data['address']['city'];
+    }
+    if (data['address']['town'] !== undefined) {
+        return data['address']['town'];
+    }
+    if (data['address']['village'] !== undefined) {
+        return data['address']['village'];
+    }
+    return 'failure\n' + data;
 }
