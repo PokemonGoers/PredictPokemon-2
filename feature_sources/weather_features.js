4c33c89f17adfee7d4926649b17d50310cd155bd
SEMIONIY-PC\Semioniy <semioniy@mail.ru>
2016-09-11 02:18:12 +0300

    Weather API for updated Core functions

--- feature_sources/weather_features.js
+++ feature_sources/weather_features.js
@@ -1,72 +1,47 @@
+var forecast = require('forecast.io')
+APIKeys = ['4e9bb2e33940272eeea09e0210886de0']
+options = { APIKey: APIKeys[0]  }   //here possible to implement switching between keys, if more than 1000 requests/day are done. counter in parent "class" needed (to switch after, say, 950 requests)
+Forecast = new forecast(options);   //init API with API key
+
+
+respond = require('../dataSet_creator.js').respond  // how to import???                       !???!
+
+
+
 (function (exports) {
     var module = exports.module = {};
-    var forecast = require('forecast.io');
-    APIKeys = ['4e9bb2e33940272eeea09e0210886de0'];
-    var options = { APIKey: APIKeys[0] };       //here possible to implement switching between keys, if more than 1000 requests/day are done. counter in parent "class" needed (to switch after, say, 950 requests)
-    Forecast = new forecast(options);           //init API with API key
-
-    /**
-     * Get the feature value for the specified key by using the data of the pokeEntry,
-     * which represents the JSON object which is returned from the API for a Pokemon sighting.
-     * @param keys array of keys which refer to the name of features. The keys are read out from the feature_config.json
-     * @param pokeEntry the JSON object which is received from the API for a Pokemon sighting
-     * @returns the values for the specified features by considering the given data.
-     */
     module.getFeatures = function (keys, pokeEntry) {
-        var values = {};
-        var date = new Date(pokeEntry.appearedLocalTime);
-        Forecast.getAtTime(pokeEntry.latitude, pokeEntry.longitude, Math.round( date.getTime()/ 1000), function (err, res, data) {//    lat/long switched
-            if (err) throw err;
-            if (data.error) {
-                console.log('Error: '  + data.error + ', code: ' + data.code);
-                throw data.error;
+        var values = {}
+        keys.forEach(function (key) {
+
+//List check! - if this value is in the list, then just read and return it; if not - calculate such
+            if (respond===undefined  || respond[pokeEntry["_id"]] === undefined) {
+                var date = new Date(pokeEntry.appearedLocalTime)
+                Forecast.getAtTime(pokeEntry.latitude, pokeEntry.longitude, Math.round(date.getTime() / 1000), function (err, res, data) {//    lat/long switched
+                    if (err) throw err;
+                    respond[pokeEntry[_id]] = [data.timezone, data.currently.summary, data.currently.precipType, ((data.currently.temperature - 32) / 1.8).toFixed(1),
+                        data.currently.humidity, data.currently.windSpeed, data.currently.windBearing, data.currently.pressure]
+                });
             }
-            /*console.log(util.inspect(data));   //for testing - possible returns
-             console.log('Latitude: ' + data.latitude);
-             console.log('Longitude: ' + data.longitude);
-             console.log('Timezone: ' + data.timezone);
-             console.log('Weather: ' + data.currently.summary);
-             console.log(data.currently.precipType);
-             console.log('Temperature: ' + ((data.currently.temperature - 32) / 1.8).toFixed(1));
-             console.log('Humidity: ' + data.currently.humidity);
-             console.log('Speed of wind: ' + data.currently.windSpeed);
-             console.log('Wind bearing: ' + data.currently.windBearing);
-             console.log('Pressure: ' + data.currently.pressure);*/
 
-            keys.forEach(function (key) {
-                if (key === "timezone") {
-                    values[key] = data.timezone;
-                }
-                else if (key === "weather") {
-                    values[key] = data.currently.summary;
-                }
-                else if (key === "precipType") {
-                    values[key] = data.currently.precipType;
-                }
-                else if (key === "temperature") {
-                    values[key] = ((data.currently.temperature - 32) / 1.8).toFixed(1);
-                }
-                else if (key === "humidity") {
-                    values[key] = data.currently.humidity;
-                }
-                else if (key === "windSpeed") {
-                    values[key] = data.currently.windSpeed;
-                }
-                else if (key === "windBearing") {
-                    values[key] = data.currently.windBearing;
-                }
-                else if (key === "pressure") {
-                    values[key] = data.currently.pressure;
-                }
-                else {
-                    console.log("The key " + key + " is not in the raw API data.");
-                    throw "UnknownFeatureKey";
-                }
-            });
-        });
-
-        return values;
-        // example: Timezone: Australia/Sydney, data.currently.summary: Partly Rain, data.currently.precipType: Rain,
-        // Temperature in Celsius, Humidity: 0.88, Speed of wind: 12.82, Wind bearing: 357, Pressure: 995.57
+            if (key === "timezone") {
+                values[key]= respond[0];
+            } else if (key === "weather") {
+                values[key]= respond[1]
+            } else if (key === "precipType") {
+                values[key]= respond[2]
+            } else if (key === "temperature") {
+                values[key]= respond[3]
+            } else if (key === "humidity") {
+                values[key]= respond[4]
+            } else if (key === "windSpeed") {
+                values[key]= respond[5]
+            } else if (key === "windBearing") {
+                values[key]= respond[6]
+            } else if (key === "pressure") {
+                values[key]= respond[7]
+            }
+        })
+        return values
     }
 })('undefined' !== typeof module ? module.exports : window);
