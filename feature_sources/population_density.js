586260caf4bd141775b84b35098f9bbd68053498
Marcel Wagenländer <marcel.wagenlaender@tum.de>
2016-09-22 15:29:22 +0200

    moved population_density.js to feature_sources and adapted it

---
+++ feature_sources/population_density.js
@@ -0,0 +1,89 @@
+(function (exports) {
+    var module = exports.module = {};
+
+    module.getFeatures = function (keys, pokeEntry) {
+        var values = {};
+        var density = parseCSV('../population_density.csv');
+
+        keys.forEach(function (key) {
+            if (key === "rural") { // 0-199
+                values[key] = isRural(getPopulationDensity(density, pokeEntry.latitude, pokeEntry.longitude));
+            }
+            else if (key === "midurban") { // 200-399
+                values[key] = isMidurban(getPopulationDensity(density, pokeEntry.latitude, pokeEntry.longitude));
+            }
+            else if (key === "suburban") { // 400-799
+                values[key] = isSuburban(getPopulationDensity(density, pokeEntry.latitude, pokeEntry.longitude));
+            }
+            else if (key === "urban") { // >=800
+                values[key] = isUrban(getPopulationDensity(density, pokeEntry.latitude, pokeEntry.longitude));
+            }
+            else {
+                console.log("The key " + key + " is not handled by the population density feature source.");
+                throw "UnknownFeatureKey";
+            }
+        });
+
+        return values;
+    };
+    
+    function isRural(number) {
+        if (number < 200) {
+            return true;
+        }
+        return false;
+    }
+
+    function isMidurban(number) {
+        if (number >= 200) {
+            return true;
+        }
+        return false;
+    }
+
+    function isSuburban(number) {
+        if (number >= 400) {
+            return true;
+        }
+        return false;
+    }
+
+    function isUrban(number) {
+        if (number >= 800) {
+            return true;
+        }
+        return false;
+    }
+
+    function getPopulationDensity (density, lat, long) {
+        // map lat long to entry point
+        var pixel_lat = 900 - (lat * 10);
+        var pixel_long = 1800 + (long * 10);
+
+        //cut decimal places
+        pixel_lat = parseInt(pixel_lat);
+        pixel_long = parseInt(pixel_long);
+
+        // unit is persons/km^2
+        return density[pixel_lat][pixel_long];
+    }
+
+    //parses a csv file into a 2D array of floats
+    function parseCSV (path) {
+        const fs = require('fs');
+
+        const data = fs.readFileSync(path, 'utf8');
+
+        var array = data.split('\n');
+        for (var i = 0; i < array.length; i++) {
+            array[i] = array[i].split(',');
+        }
+
+        for (var i = 0; i < array.length; i++) {
+            for (var j = 0; j < array[i].length; j++) {
+                array[i][j] = parseFloat(array[i][j]);
+            }
+        }
+        return array;
+    }
+})('undefined' !== typeof module ? module.exports : window);
