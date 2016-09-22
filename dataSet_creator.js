123d238ff60bbedbe193fc18b6234dc74b5fc1a3
MatthiasBaur <baurm@in.tum.de>
2016-09-22 16:34:42 +0200

    Migration of the cooccurence feature #2

--- dataSet_creator.js
+++ dataSet_creator.js
@@ -173,23 +173,70 @@ CachedWeatherResponses = {"empty":"json file"};//for API Request results storage
                 });
             });
         });
-        
+        if (addCooccurence) {
+            //add cooc labels
+            for (var i = 1; i <= 151; i++) {
+                arff += "@ATTRIBUTE cooc_" + i + " {false, true}\n";
+            }
 
+            //init the datastructure for cooc
+            var cooc = [];
+            cooc = initCoocs(dataSet);
+            //compute the cooccurence
+            computeCooc(cooc);
+        }
         // add the class label
         var classLabels = allValuesForKeyInData(classSource.classKey, dataSet);
         arff += '@ATTRIBUTE class {' + classLabels.join(', ') + '}\n\n';
 
         // add the data
         arff += '@DATA\n';
-        dataSet.forEach(function (dataRow) {
-            var values = [];
-            for (var key in dataRow) {
-                values.push(dataRow[key])
-            }
-            arff += values.join(',') + '\n';
-        });
 
-        fs.writeFileSync(fileNamePath, arff, 'utf8');
+        if(!addCooccurence) {
+            dataSet.forEach(function (dataRow) {
+                var values = [];
+                for (var key in dataRow) {
+                    values.push(dataRow[key])
+                }
+                arff += values.join(',') + '\n';
+            });
+
+            fs.writeFileSync(fileNamePath, arff, 'utf8');
+        } else {
+            fs.writeFileSync(fileNamePath, arff, 'utf8');
+            if(consoleOn) console.log("Wrote the header.");
+            var len = dataSet.length;
+            var values = [];
+            var row = null;
+            for(var i=0; i<len;i++){
+                values = [];
+                //all the features
+                row = dataSet.shift();
+                //cooccurences
+                for(var i = 1; i <=151; i++){
+                    row['cooc_' + i] = (cooc[iter]["cooccurCellId90_" + (32*Math.floor(i/32))] & (1<<(i%32)))!==0;
+                }
+                //class labels
+                row[classSource.classKey] = classLables.shift();
+
+                //extract values
+                for (var key in row) {
+                    values.push(row[key])
+                }
+                arff += values.join(',') + '\n';
+                if(i%5000===0||iter===(len-1)){
+                    console.log("Writing...");
+                    if (i===(len-1)){
+                        arff=arff.slice(0,-2);
+                        fs.appendFileSync(fileNamePath, arff, 'utf8');
+                    } else {
+                        fs.appendFileSync(fileNamePath, arff, 'utf8');
+                        arff = "";
+                    }
+                    if(consoleOn) console.log("" + i + " instances written.");
+                }
+            }
+        }
     }
 
     /**
@@ -260,4 +307,101 @@ CachedWeatherResponses = {"empty":"json file"};//for API Request results storage
         var data = fs.readFileSync(file, 'utf8');
         return JSON.parse(data);
     }
+
+
+    /**
+     * Initializes the data structure for the cooccurence to work on
+     * that is: 5 Integers to hold the "boolean" values
+     * @param _data The data for which the cooccurence is added
+     * @returns {Array} data struc for cooccurence
+     */
+    function initCoocs (_data){
+        var coocData = [];
+        //iter to run along the forEach loop and fethc the labels.
+        var iter = 0;
+        _data.forEach(function(row) {
+            var name = "cooccurCellId90_";
+            var new_row = {};
+            new_row['_id'] = row['_id'];
+            new_row['cellId_90m'] = row['cellId_90m'];
+            new_row['appearedHour'] = row['appearedHour'];
+            new_row['appearedDay'] = row['appearedDay'];
+            new_row['appearedMonth'] = row['appearedMonth'];
+            new_row['pokemonId'] = classLables[iter];
+            new_row[name + "0"] = 0;
+            new_row[name + "32"] = 0;
+            new_row[name + "64"] = 0;
+            new_row[name + "96"] = 0;
+            new_row[name + "128"] = 0;
+            coocData.push(new_row);
+            iter ++;
+        });
+        console.log("Initialized cooc var with " + coocData.length + " rows.");
+        return coocData;
+    }
+
+    /**
+     * traverses the data and adds cooccurence, if the sighting was in the same cell
+     * and in the same timeframe
+     * @param _data the cooc-structure to work on
+     */
+    function computeCooc (_data){
+        var count_cooc=0;
+        var count_cell=0;
+        var sum = (_data.length+1)*_data.length/2;
+        for(var i = 0;i <_data.length; i++){
+            if(i%100 == 0){
+                var current = i*100/_data.length;
+                console.log("Roughly at " + current.toFixed(2) + "% of co-occurence computations with " + count_cooc +
+                    " co-occurrences in "+ count_cell + " cell hits.");
+            }
+            for(var j = 0; j<_data.length; j++){
+                if(i!=j) {
+                    if (_data[i].cellId_90m === _data[j].cellId_90m) {
+                        count_cell++;
+                        if (within24(_data[i], _data[j])) {
+                            if (_data[i].pokemonId !== _data[j].pokemonId) {
+                                insert_id_to_int(_data[i], _data[j]);
+                                count_cooc++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    /**returns whether or not the sighting data2 was within the hour before data1
+     *
+     * @param The tuple the cooccurence will be inserted in
+     * @param The tuple, which is in the same cell.
+     * @returns {boolean} whether the occurence was within the same hour / hour before
+     */
+    function within24(data1, data2){
+        //console.log(data1.appearedHour + " and " + data2.appearedHour + " and "
+        //    +data1.class + " and " + data2.class);
+        if(data1.appearedHour==data2.appearedHour
+            ||(data1.appearedHour-1)==data2.appearedHour
+            ||(data1.appearedHour==0&&data2.appearedHour==23)){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    /**
+     * Magic. Do not touch.
+     *
+     * @param data_1 The tuple the information is stored in
+     * @param data_2 The tuple that contains the cooccurence
+     */
+    function insert_id_to_int(data_1, data_2){
+        var str = 'cooccurCellId90_';
+        var int_1 = Math.floor(data_1.pokemonId/32);
+        var int_2 = Math.floor(data_2.pokemonId/32);
+        data_2[str + (int_1*32)] = data_2[str + (int_1*32)] | (1<<(data_1.pokemonID%32));
+        data_1[str + (int_2*32)] = data_1[str + (int_2*32)] | (1<<(data_2.pokemonId%32));
+    }
+
 })('undefined' !== typeof module ? module.exports : window);
