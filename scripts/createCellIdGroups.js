fd3e63ec6c24a16cf0139b97bffb6ded7d50d845
Benjamin Strobel <benjamin.strobel@tum.de>
2016-09-23 00:00:12 +0200

    added proximity to gym features

--- scripts/createCellIdGroups.js
+++ scripts/createCellIdGroups.js
@@ -5,7 +5,8 @@
 var S2 = require('s2-geometry').S2;
 var fs = require('fs');
 
-const s2levels = [1,2,3,4,6,8,10,12,13,14,17];
+// add two times 1 to handle face of s2 key separately from s2 key parts
+const s2levels = [1,1,2,3,4,6,8,10,12,13,14,17];
 const lastLevel = s2levels[s2levels.length - 1];
 var s2levelIndices = s2levels.reduce(function(object, value, i) {
     object[i] = value;
@@ -29,7 +30,17 @@ function createCellIdGroups(sourcePath,destinationPath) {
 }
 
 function addPlace(place, currentGroup, levelIndex) {
-    var key = S2.keyToId(S2.latLngToKey(place.latitude, place.longitude, s2levelIndices[levelIndex]));
+    var key = S2.latLngToKey(place.latitude, place.longitude, s2levelIndices[levelIndex]);
+    var parts = key.split('/');
+    var face = parseInt(parts[0]);
+    var position = parts[1];
+
+    if (levelIndex === 0) {
+        key = face;
+    }
+    else {
+        key = position;
+    }
 
     if (s2levelIndices[levelIndex] !== lastLevel) {
         if (!currentGroup.hasOwnProperty(key)) {
@@ -66,7 +77,7 @@ function printCellCount(places, indent) {
     {
         text = indent + Object.keys(places).length;
         for(key in places) {
-            text += '\n' + printPlaceCount(places[key], indent + '\t');
+            text += '\n' + printCellCount(places[key], indent + '\t');
         }
         return text;
     }
@@ -77,5 +88,5 @@ createCellIdGroups('../json/gyms.json', '../json/gym_groups.json');
 // create log file to see cell counts
 var file = fs.readFileSync('../json/gym_groups.json', 'utf8');
 var cellGroups = JSON.parse(file);
-var text = printPlaceCount(cellGroups.places, '');
+var text = printCellCount(cellGroups.places, '');
 fs.writeFileSync('../json/cellGroup.log', text, 'utf8');
