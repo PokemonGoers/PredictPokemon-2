569a2d6c765d712836738da9a65deda122c4a0c5
aurel <aurelroci@hotmail.com>
2016-10-04 23:29:55 +0200

    Revert "prediction using weka"
    This reverts commit 2ad86da96dbdb5fae23485c6c8b9957e4a85e97e.

--- getApiData.js
+++ getApiData.js
@@ -1,23 +1,22 @@
 var fs = require('fs');
 var DS = require('./dataSet_creator.js').DC;
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var url = 'http://pokedata.c4e3f8c7.svc.dockerapp.io:65014/api/pokemon/sighting/';
+var destination = 'arff/apiDataExtended.arff';
 
+httpGetAsync(url, destination);
 
-// httpGetAsync(url, destination);
-
-function httpGetAsync(url, destination, callback) {
-    console.log('requesting ' + url);
+function httpGetAsync(url, destination) {
+    console.log('requesting ' +url);
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = function () {
         if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
             var apiData = JSON.parse(xmlHttp.responseText);
-            console.log('downloaded ' + apiData.data.length + ' sightings from API');
+            console.log('downloaded ' +apiData.data.length + ' sightings from API');
             DS.storeArffFile("feature_config.json", apiData.data.slice(0, 121), destination);
         }
     };
     xmlHttp.open("GET", url, true);
     xmlHttp.send();
-    callback();
+    return xmlHttp.responseText;
 }
-
-module.exports = {get: httpGetAsync};
