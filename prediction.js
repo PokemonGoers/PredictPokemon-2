fb2a6539cf03836bc5f8a1ce50117fef210b818f
Benjamin Strobel <benjamin.strobel@tum.de>
2016-10-31 09:57:21 +0100

    removed merge confilt code

--- prediction.js
+++ prediction.js
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-var data = require('./getApiData');
-var weka = require('node-weka');
-
-var url = 'http://pokedata.c4e3f8c7.svc.dockerapp.io:65014/api/pokemon/sighting/';
-var destination = 'arff/apiDataExtended.arff';
-
-function startPrediction() {
-
-    data.get(url, destination, function () {
-        var data = destination;
-
-        var options = {
-            'classifier': 'weka.classifiers.bayes.BayesNet',
-            'params': ''
-        };
-
-        weka.classify(data, testData, options, function (err, result) {
-        });
-    });
-}
-
-module.exports = {predict: startPrediction};
-=======
 /**
  * Created on 07.10.2016.
  */
@@ -263,4 +239,3 @@ module.exports = {predict: startPrediction};
         console.log(new Date().toJSON() + ' ' + message);
     }
 })('undefined' !== typeof module ? module.exports : window);
->>>>>>> develop
