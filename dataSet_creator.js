417e925fc6af69bceb6b9549cb48488a749a82e9
Aurel Roci <aurel.roci@tum.de>
2016-12-22 10:12:56 +0100

    check for undefined pokemonId

--- dataSet_creator.js
+++ dataSet_creator.js
@@ -347,7 +347,7 @@ WeatherApiKeyCounter=0;
         var complete_data = [];
 
         json_data.forEach(function (element) {
-            if (element["location"] !== null && element["pokemonId"] !== null) {
+            if (element["location"] !== null && element["pokemonId"] !== null  && typeof element["pokemonId"] !== "undefined") {
                 complete_data.push(element);
             }
         });
--- getApiData.js
+++ getApiData.js
@@ -3,33 +3,17 @@ var DC = require('./dataSet_creator.js').DC;
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 
-storeApiData();
 
-<<<<<<< HEAD
 function httpGetAsync(url, destination, callback) {
     console.log('requesting ' + url);
-=======
-function storeApiData() {
-    var data = getData(function (data) {
-        DC.init("feature_config.json", true);
-        DC.storeArffFile(data.slice(0, 121), destination, true);
-    });
-}
 
-function getData(callback) {
-    console.log('requesting ' +url);
->>>>>>> develop
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = function () {
         if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
             var apiData = JSON.parse(xmlHttp.responseText);
-<<<<<<< HEAD
             console.log('downloaded ' + apiData.data.length + ' sightings from API');
             DS.storeArffFile("feature_config.json", apiData.data.slice(0, 121), destination);
-=======
-            console.log('downloaded ' +apiData.data.length + ' sightings from API');
-            callback(apiData.data);
->>>>>>> develop
+
         }
     };
     xmlHttp.open("GET", url, true);
